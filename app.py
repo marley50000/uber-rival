4ce112e96f8a3a67f46eda6469c26b670342d9e8
marley50000 <152743191+marley50000@users.noreply.github.com>
2025-03-10 22:32:10 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ def serve_logo():
 
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    serve(app, host='0.0.0.0', port=port)
